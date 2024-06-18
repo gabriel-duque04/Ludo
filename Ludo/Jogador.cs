cb1c9904e408357eff5dc1b07bfc457351ae93d2
INF-GABRIEL <INF-GABRIEL@INF-GABRIEL>
2024-06-18 17:05:53 -0300

    Adicionar arquivos de projeto.

---
+++ Ludo/Jogador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo
+{
+    internal class Jogador
+    {
+        public string cor;
+        public int id;
+        public string nome;
+        public Peao[] peoes;
+    
+        public Jogador(string cor, int id,string nome)
+        {
+            this.cor = cor;
+            this.id = id;
+            this.nome = nome;
+            this.peoes = new Peao[4];
+        }
+        public int lancaDado()
+        {
+            Random r = new Random();
+            return (r.Next(1,7));
+        }
+        public void moverPeao(int id)
+        {
+            posicao += (lancaDado());
+        }
+    }
+}
---
+++ Ludo/Peao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo
+{
+    internal class Peao
+    {
+        private string peao;
+        private int id;
+        private int posicao;
+
+        public Peao(string peao, int id, int posicao)
+        {
+            this.peao = peao;
+            this.id = id;
+            this.posicao = posicao;
+        }
+
+        public void Mover(int casas)
+        {
+            posicao += casas;
+        }
+        public void retornarCasa() { 
+            posicao = 0;
+        }
+    }
+}
